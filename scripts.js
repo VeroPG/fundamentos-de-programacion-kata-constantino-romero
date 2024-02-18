0bbcd547910e6c3e3410de206df11a9782231388
Veronica Parra <vero.parragraca@gmail.com>
2024-02-18 20:36:40 +0100

    Completado

--- scripts.js
+++ scripts.js
@@ -35,7 +35,7 @@ const pregunta = {
 }
 
 function imprimePregunta(pregunta) {
-  const newHTML = "";
+  let newHTML = "";
 
   newHTML += imprimeTitulo(pregunta);
   newHTML += imprimeTodasLasRespuestas(pregunta);
@@ -44,23 +44,40 @@ function imprimePregunta(pregunta) {
 }
 
 function imprimeTitulo(pregunta) {
-  // Put your code here
+  return `<p>${pregunta.titulo}</p>`
 }
 
 function imprimeTodasLasRespuestas(pregunta) {
-  // Put your code here
+  
+
+  let resultado = "";
+  let longitudObjeto = Object.keys(pregunta.respuestas).length;
+  
+
+  for (let i = 0; i < longitudObjeto; i++) {
+    let result= pregunta.respuestas[i];
+    resultado += imprimeUnaRespuesta(result)
+  }
+
+return resultado;
 }
 
 function imprimeUnaRespuesta(respuesta) {
-  // Put your code here
+  return ` ${imprimeLabel(respuesta)}  ${imprimeInput(respuesta)}`
+
 }
 
 function imprimeLabel(respuesta) {
-  // Put your code here
+  return `<label for= ${respuesta.id} > ${respuesta.label} </label>`;
+  
+
 }
 
 function imprimeInput(respuesta) {
-  // Put your code here
+
+  return `<input type="radio" id="${respuesta.id}" name="${respuesta.name}" value="${respuesta.value}"></input>`
 }
 
+
 document.getElementById("contenedorPreguntas").innerHTML = imprimePregunta(pregunta);
+
